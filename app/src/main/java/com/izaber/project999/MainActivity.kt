5f4e0b62fb92cd1b5d71fcb9f255f23ccaf168d4
Ilya Zaberezhnyy <ilya.zaber@gmail.com>
2023-09-03 12:04:36 +0600

    add btn

--- app/src/main/java/com/izaber/project999/MainActivity.kt
+++ app/src/main/java/com/izaber/project999/MainActivity.kt
@@ -2,10 +2,18 @@ package com.izaber.project999
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<AppCompatButton>(R.id.button_show_toast)
+        btn.setOnClickListener {
+
+        }
+        Toast.makeText(this@MainActivity, "Hello world", Toast.LENGTH_LONG).show()
     }
 }
