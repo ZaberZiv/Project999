84acaaff131fe394df295b2c6241512ee6f9706d
Ilya Zaberezhnyy <ilya.zaber@gmail.com>
2023-10-07 20:59:10 +0600

    death process handler

--- app/src/main/java/com/izaber/project999/MainActivity.kt
+++ app/src/main/java/com/izaber/project999/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.izaber.project999
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import com.izaber.project999.app.App
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (application as App).activityCreated(savedInstanceState == null)
 
         val btn = findViewById<AppCompatButton>(R.id.button_show_toast)
         btn.setOnClickListener {
---
+++ app/src/main/java/com/izaber/project999/app/App.kt
@@ -0,0 +1,31 @@
+package com.izaber.project999.app
+
+import android.app.Application
+import android.util.Log
+
+class App: Application() {
+    private val tag = "APP"
+    private val deathHandler: ProcessDeathHandler = ProcessDeathHandler.Base()
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.v(tag, "App created")
+    }
+
+    fun activityCreated(firstOpening: Boolean) {
+        if (firstOpening) {
+            // init local cache
+            Log.v(tag, "Very first opening")
+            deathHandler.firstOpening()
+        } else {
+            if (deathHandler.wawDeathHappened()) {
+                // go to permanent storage and init localCache
+                Log.v(tag, "Process death happened")
+                deathHandler.deathHandled()
+            } else {
+                // use local cache and don't use permanent
+                Log.v(tag, "Activity recreated")
+            }
+        }
+    }
+}
